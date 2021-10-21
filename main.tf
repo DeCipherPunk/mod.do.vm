6487c180ba2fa17e686942141627d76a91b810ab
DeCipherPunk <decipherpunk@protonmail.com>
2021-10-21 19:53:21 +0530

    fix var typo

--- main.tf
+++ main.tf
@@ -22,5 +22,5 @@ resource "digitalocean_droplet" "vm" {
   ipv6 = "${var.vm_ipv6}"
   resize_disk = "${var.vm_resize_disk}"
   user_data = "${var.vm_bootstrap_data}"
-  tags = "${stack_tags}"
+  tags = "${var.stack_tags}"
 }
