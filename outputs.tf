ff30cfc2b62bca44f6fd636dd10fbaa0913c34e3
DeCipherPunk <decipherpunk@protonmail.com>
2021-10-21 20:00:44 +0530

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,76 +1,76 @@
 output "vm_id" {
-  value = digitalocean_droplet.vm[count.index].id
+  value = digitalocean_droplet.vm.*.id
 }
 
 output "vm_name" {
-  value = digitalocean_droplet.vm[count.index].name
+  value = digitalocean_droplet.vm.*.name
 }
 
 output "vm_urn" {
-  value = digitalocean_droplet.vm[count.index].urn
+  value = digitalocean_droplet.vm.*.urn
 }
 
 output "vm_region" {
-  value = digitalocean_droplet.vm[count.index].region
+  value = digitalocean_droplet.vm.*.region
 }
 
 output "vm_image" {
-  value = digitalocean_droplet.vm[count.index].image
+  value = digitalocean_droplet.vm.*.image
 }
 
 output "vm_ipv6" {
-  value = digitalocean_droplet.vm[count.index].ipv6
+  value = digitalocean_droplet.vm.*.ipv6
 }
 
 output "vm_ipv6_address" {
-  value = digitalocean_droplet.vm[count.index].ipv6_address
+  value = digitalocean_droplet.vm.*.ipv6_address
 }
 
 output "vm_ipv4_address" {
-  value = digitalocean_droplet.vm[count.index].ipv4_address
+  value = digitalocean_droplet.vm.*.ipv4_address
 }
 
 output "vm_ipv4_address_private" {
-  value = digitalocean_droplet.vm[count.index].ipv4_address_private
+  value = digitalocean_droplet.vm.*.ipv4_address_private
 }
 
 output "vm_locked" {
-  value = digitalocean_droplet.vm[count.index].locked
+  value = digitalocean_droplet.vm.*.locked
 }
 
 output "vm_private_networking" {
-  value = digitalocean_droplet.vm[count.index].private_networking
+  value = digitalocean_droplet.vm.*.private_networking
 }
 
 output "vm_price_hourly" {
-  value = digitalocean_droplet.vm[count.index].price_hourly
+  value = digitalocean_droplet.vm.*.price_hourly
 }
 
 output "vm_price_monthly" {
-  value = digitalocean_droplet.vm[count.index].price_monthly
+  value = digitalocean_droplet.vm.*.price_monthly
 }
 
 output "vm_size" {
-  value = digitalocean_droplet.vm[count.index].size
+  value = digitalocean_droplet.vm.*.size
 }
 
 output "vm_disk" {
-  value = digitalocean_droplet.vm[count.index].disk
+  value = digitalocean_droplet.vm.*.disk
 }
 
 output "vm_vcpus" {
-  value = digitalocean_droplet.vm[count.index].vcpus
+  value = digitalocean_droplet.vm.*.vcpus
 }
 
 output "vm_status" {
-  value = digitalocean_droplet.vm[count.index].status
+  value = digitalocean_droplet.vm.*.status
 }
 
 output "vm_tags" {
-  value = digitalocean_droplet.vm[count.index].tags
+  value = digitalocean_droplet.vm.*.tags
 }
 
 output "vm_volume_ids" {
-  value = digitalocean_droplet.vm[count.index].volume_ids
+  value = digitalocean_droplet.vm.*.volume_ids
 }
 
